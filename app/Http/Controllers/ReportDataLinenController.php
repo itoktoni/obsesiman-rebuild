724c2cd40e1aa57158b978d6909a0360a12cf44b
itoktoni <itok.toni@gmail.com>
2023-12-02 17:38:30 +0700

    fix export excel

--- app/Http/Controllers/ReportDataLinenController.php
+++ app/Http/Controllers/ReportDataLinenController.php
@@ -8,8 +8,8 @@ use App\Dao\Models\Rs;
 use App\Dao\Models\Ruangan;
 use App\Dao\Models\ViewDetailLinen;
 use App\Dao\Repositories\DetailRepository;
-use App\Jobs\downloadReport;
 use Illuminate\Http\Request;
+use Spatie\SimpleExcel\SimpleExcelWriter;
 
 class ReportDataLinenController extends MinimalController
 {
@@ -39,15 +39,49 @@ class ReportDataLinenController extends MinimalController
         return self::$repository->getPrintDataMaster()->get();
     }
 
+    private function exportExcel($request) {
+        $writer = SimpleExcelWriter::streamDownload('your-export.xlsx');
+        self::$repository->getPrintDataMaster()->chunk(1000, function($item) use ($writer){
+            foreach($item as $key => $table){
+                $writer->addRow([
+                    'No.' => $key+1,
+                    'NO. RFID' => $table->field_primary,
+                    'KATEGORI LINEN' => $table->view_kategori_nama,
+                    'LINEN' => $table->field_name,
+                    'BERAT' => $table->field_weight,
+                    'RUMAH SAKIT' => $table->field_rs_name,
+                    'RUANGAN' => $table->field_ruangan_name,
+                    'CUCI/RENTAL' => $table->field_status_cuci_name,
+                    'JUMLAH BERSIH' => $table->field_bersih,
+                    'JUMLAH RETUR' => $table->field_retur,
+                    'JUMLAH REWASH' => $table->field_rewash,
+                    'POSISI TERAKHIR' => $table->field_status_process_name,
+                    'TGL POSISI TERAKHIR' => formatDate($table->field_tanggal_update),
+                    'OPERATOR UPDATE TERAKHIR' => $table->field_updated_name,
+                    'STATUS REGISTER' => $table->field_status_register_name,
+                    'TGL REGISTRASI' => formatDate($table->field_tanggal_create),
+                    'OPERATOR REGISTRASI' => $table->field_created_name,
+                ]);
+
+                if ($key % 1000 === 0) {
+                    flush(); // Flush the buffer every 1000 rows
+                }
+            }
+        });
+
+        return $writer->toBrowser();
+    }
+
     public function getPrint(Request $request){
         set_time_limit(0);
         $rs = Rs::find(request()->get(ViewDetailLinen::field_rs_id()));
 
         $this->data = [];
-        if($request->action != 'export'){
-            $this->data = $this->getQuery($request);
+        if($request->action == 'export'){
+            return $this->exportExcel($request);
         }
 
+        $this->data = $this->getQuery($request);
         return moduleView(modulePathPrint(), $this->share([
             'data' => $this->data,
             'rs' => $rs,
