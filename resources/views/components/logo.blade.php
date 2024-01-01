0065326704209f43ff686b96cb986502e25a124a
itoktoni <itok.toni@gmail.com>
2024-01-02 00:52:43 +0700

    update

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,1 +1,8 @@
-<img style="position: absolute;left:40%;top:5px;height:100px" src="{{ env('APP_LOGO') ? url('storage/'.env('APP_LOGO')) : url('assets/media/image/logo.png') }}" alt="logo">
+@php
+$action = request()->get('action');
+$height = '100px';
+if($action == 'excel'){
+    $height = '15%';
+}
+@endphp
+<img style="position: absolute;left:40%;top:5px;height:{{ $height }}" src="{{ env('APP_LOGO') ? url('storage/'.env('APP_LOGO')) : url('assets/media/image/logo.png') }}" alt="logo">
