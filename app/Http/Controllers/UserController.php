acc7ca87dae8dff492408a3d411a52453501e02a
itoktoni <itok.toni@gmail.com>
2024-02-04 10:42:29 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,7 +69,9 @@ class UserController extends MasterController
             ],'Login Gagal');
         }
 
-        $user->tokens()->delete();
+        if($user->tokens()){
+            $user->tokens()->delete();
+        }
 
         $token = $user->createToken($user->name);
         $string_token = $token->plainTextToken;
