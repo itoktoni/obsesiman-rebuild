6c290b2279a727c02c1939a026a60e0b5413eb99
itoktoni <itok.toni@gmail.com>
2024-05-01 12:48:32 +0700

    update sort opname rekap

--- app/Http/Controllers/ReportRekapOpnameController.php
+++ app/Http/Controllers/ReportRekapOpnameController.php
@@ -57,7 +57,7 @@ class ReportRekapOpnameController extends MinimalController
 
         if ($rs) {
             $location = $opname->pluck('ruangan_nama', 'ruangan_id')->unique();
-            $linen = $opname->pluck('jenis_nama', 'jenis_id')->unique();
+            $linen = $opname->sortBy('jenis_nama')->pluck('jenis_nama', 'jenis_id')->unique();
         }
 
         return moduleView(modulePathPrint(), $this->share([
