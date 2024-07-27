64476e81f55d46cf5cd2174e096b4c948c845228
Itok toni laksono <itok.toni@gmail.com>
2024-07-27 22:02:32 +0700

    update detail total

--- app/Console/Commands/DetailTotal.php
+++ app/Console/Commands/DetailTotal.php
@@ -2,15 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Dao\Enums\ProcessType;
 use App\Dao\Models\Detail;
-use App\Dao\Models\Transaksi;
-use App\Dao\Models\ViewDetailLinen;
 use Illuminate\Console\Command;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Plugins\History;
 
 class DetailTotal extends Command
 {
@@ -46,27 +40,32 @@ class DetailTotal extends Command
      */
     public function handle()
     {
-       $data = DB::table('view_transaksi_total')
-        ->where('detail_tanggal_cek', '!=', date('Y-m-d'))
-        ->orWhereNull('detail_tanggal_cek')
-        ->limit(ENV('TRANSACTION_DETAIL_CEK', 50))
-        ->get();
+        try {
+            //code...
 
-        Log::info($data);
+            $data = DB::table('view_transaksi_total')
+                ->where('detail_tanggal_cek', '!=', date('Y-m-d'))
+                ->orWhereNull('detail_tanggal_cek')
+                ->limit(ENV('TRANSACTION_DETAIL_CEK', 50))
+                ->get();
 
-        if ($data) {
-            foreach($data as $item){
-                Detail::where(Detail::field_primary(), $item->detail_rfid)->update([
-                    Detail::field_total_bersih_kotor() => $item->qty_bersih_kotor,
-                    Detail::field_total_bersih_retur() => $item->qty_bersih_retur,
-                    Detail::field_total_bersih_rewash() => $item->qty_bersih_rewash,
-                    Detail::field_total_kotor() => $item->qty_kotor,
-                    Detail::field_total_retur() => $item->qty_retur,
-                    Detail::field_total_rewash() => $item->qty_rewash,
-                    Detail::field_total_cuci() => $item->qty_cuci,
-                    Detail::field_cek() => date('Y-m-d'),
-                ]);
+            if ($data) {
+                foreach ($data as $item) {
+                    $check = Detail::where(Detail::field_primary(), $item->detail_rfid)->update([
+                        Detail::field_total_bersih_kotor() => $item->qty_bersih_kotor,
+                        Detail::field_total_bersih_retur() => $item->qty_bersih_retur,
+                        Detail::field_total_bersih_rewash() => $item->qty_bersih_rewash,
+                        Detail::field_total_kotor() => $item->qty_kotor,
+                        Detail::field_total_retur() => $item->qty_retur,
+                        Detail::field_total_rewash() => $item->qty_rewash,
+                        Detail::field_total_cuci() => $item->qty_cuci,
+                        Detail::field_cek() => date('Y-m-d'),
+                    ]);
+                }
             }
+
+        } catch (\Throwable $th) {
+            //throw $th;
         }
 
         $this->info('The system has been check successfully!');
