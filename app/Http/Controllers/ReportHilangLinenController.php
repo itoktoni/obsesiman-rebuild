a5829b161e1305fad22a93aa84dcf3659e88440a
itoktoni <itok.toni@gmail.com>
2024-08-07 20:10:32 +0700

    update

--- app/Http/Controllers/ReportHilangLinenController.php
+++ app/Http/Controllers/ReportHilangLinenController.php
@@ -55,7 +55,7 @@ class ReportHilangLinenController extends MinimalController
             $query = $query->whereDate(ViewDetailLinen::field_hilang_create(), '<=', $end_date);
         }
 
-        return $query->showSql()->get();
+        return $query->get();
     }
 
     public function getPrint(Request $request)
