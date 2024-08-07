f7d604e823fc71668da47c05d58cfc950897c7fb
itoktoni <itok.toni@gmail.com>
2024-08-07 23:47:59 +0700

    update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
-
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 class WebhookController extends Controller
 {
     public function deploy(Request $request)
@@ -15,17 +16,18 @@ class WebhookController extends Controller
         $githubHash = $request->header('X-Hub-Signature');
         $localToken = env('GITHUB_WEBHOOK_SECRET');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-
         if (hash_equals($githubHash, $localHash)) {
 
-            $branch = env('GITHUB_WEBHOOK_BRANCH');
-            $root_path = base_path();
-            $process = shell_exec("cd {$root_path} && git checkout {$branch} && git pull");
-            Log::info($process);
-        }
-        else
-        {
-            Log::error('Kunci deploy tidak sama dengan yang ada di github');
+            chdir(base_path());
+            $process = new Process(['git', 'pull']);
+            $process->run();
+
+            // executes after the command finishes
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            return $process->getOutput();
         }
     }
 }
