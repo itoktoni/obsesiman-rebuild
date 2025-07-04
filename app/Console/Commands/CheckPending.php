9e57f32c2b42e97f6075ccb8b06d10c2adbc9861
Itok toni laksono <itok.toni@gmail.com>
2025-07-04 11:23:04 +0700

    update change to transaksi kotor created at

--- app/Console/Commands/CheckPending.php
+++ app/Console/Commands/CheckPending.php
@@ -49,7 +49,7 @@ class CheckPending extends Command
         $outstanding = Transaksi::query()
             ->select(Transaksi::field_rfid(), ViewDetailLinen::field_status_terakhir())
             ->joinRelationship(HAS_DETAIL)
-            ->where(ViewDetailLinen::field_tanggal_update(), '<=', Carbon::now()->subMinutes(1440)->toDateTimeString())
+            ->where(Transaksi::field_created_at(), '<=', Carbon::now()->subMinutes(1440)->toDateTimeString())
             ->whereNull(Transaksi::field_status_bersih())
             ->whereNot(ViewDetailLinen::field_status_trasaction(), TransactionType::Register)
             ->whereNotIn(ViewDetailLinen::field_status_trasaction(), BERSIH)
