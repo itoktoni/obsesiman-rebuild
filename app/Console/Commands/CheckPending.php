790b9db12343b151f1f52e79fac9b396c1bfe15e
itok toni laksono <itok.toni@gmail.com>
2025-07-12 17:54:23 +0700

    update pending after barcode

--- app/Console/Commands/CheckPending.php
+++ app/Console/Commands/CheckPending.php
@@ -51,6 +51,8 @@ class CheckPending extends Command
             ->where(Transaksi::field_created_at(), '<=', Carbon::now()->subMinutes(1440)->toDateTimeString())
             ->where(Transaksi::field_status_transaction(), '!=', TransactionType::Register)
             ->whereNull(Transaksi::field_report())
+            ->whereNull(Transaksi::field_barcode())
+            ->whereNull(Transaksi::field_status_bersih())
             ->whereNull(Transaksi::field_pending_in())
             ->limit(env('TRANSACTION_CHUNK', 200))
             ->get();
