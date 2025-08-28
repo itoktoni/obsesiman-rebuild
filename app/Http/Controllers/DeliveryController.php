47e294edd04cef1af7ab2df140b1dc4b8482a6cc
itok toni laksono <itok.toni@gmail.com>
2025-08-28 10:52:02 +0700

    update pending

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -197,7 +197,7 @@ class DeliveryController extends MasterController
 
             foreach($data as $key => $item){
                 $return[] = [
-                    'id' => $key.uniqid(),
+                    'id' => $key,
                     'nama' => $item[0]['nama'],
                     'lokasi' => $item[0]['lokasi'],
                     'total' => count($item),
